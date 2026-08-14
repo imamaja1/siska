d3b3d33b18d03e456068efa701cae7240ddef808
imam <you@example.com>
2026-08-14 11:03:56 +0800

    solusi session

--- application/config/config.php
+++ application/config/config.php
@@ -382,10 +382,10 @@ $config['encryption_key'] = 'dX7pQm2sL9vR5nK8wY1jF3cH6tA0bE4g';
 $config['sess_driver'] = 'database';
 $config['sess_save_path'] = 'ci_sessions';
 $config['sess_cookie_name'] = 'ci_session';
-$config['sess_expiration'] = 7200;
+$config['sess_expiration'] = 86400;
 $config['sess_match_ip'] = FALSE;
-$config['sess_time_to_update'] = 300;
-$config['sess_regenerate_destroy'] = TRUE;
+$config['sess_time_to_update'] = 1800;
+$config['sess_regenerate_destroy'] = FALSE;
 $config['sess_match_useragent'] = FALSE;
 
 /*
